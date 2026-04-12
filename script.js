b6f62c607636b9ef5604f3affb2a663bbe67bb08
Buriak-byte <oleksii.buriak12@gmail.com>
2026-04-12 22:18:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 //1
-// Напиши скрипт, який, для об'єкта user, послідовно:
-
-
-
-// додає поле mood зі значенням 'happy'
-// замінює значення hobby на 'skydiving'
-// замінює значення premium на false
-// виводить вміст об'єкта user в форматі ключ:значення використовуючи Object.keys() і for...of
 let user = {
-    hobby: football,
+  hobby: "football",
+  premium: true,
+};
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+const keys = Object.keys(user);
+
+for (const key of keys) {
+  console.log(`${key}: ${user[key]}`);
 }
-user.mood = "happy"
-user.hobby = "skydiving  
-console.log(user)
+//2
+// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
+const user2 = {
+car: "Toyota",
+home: "flat",
+placeOflive: "Kyiv",
+children: false,
+cat: true,
+countProps(obj){
+return Object.keys(obj).length;
+}
+}
+console.log(user2.countProps(user2))
+//3
+// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
+const employees = {
+    Andriy: 45,
+    Olena: 128,
+    Nazar: 150,
+    Vlad: 0,
+    Anastasia: 95,
+    findBestEmployee(employees){
+        for (const employer of employees) {
+          console.log(employer)
+        }
+    }
+}
+console.log(employees.findBestEmployee(employees));

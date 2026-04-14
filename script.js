8ccefd7ae92f96e9786906a36ac2c4f8c55cb9ad
Buriak-byte <oleksii.buriak12@gmail.com>
2026-04-14 17:48:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,16 +26,42 @@ return Object.keys(obj).length;
 console.log(user2.countProps(user2))
 //3
 // Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
+function findBestEmployee(employees) {
+  let theBestEmployer = "";
+  const employersKeys = Object.keys(employees);
+  for (const employer of employersKeys) {
+    if (employersKeys[employer] > theBestEmployer) {
+      employer = theBestEmployer;
+      console.log(`${theBestEmployer}: ${employees.values}`);
+    }
+  }
+}
 const employees = {
     Andriy: 45,
     Olena: 128,
     Nazar: 150,
     Vlad: 0,
     Anastasia: 95,
-    findBestEmployee(employees){
-        for (const employer of employees) {
-          console.log(employer)
-        }
-    }
+    
 }
-console.log(employees.findBestEmployee(employees));
+console.log(findBestEmployee(employees));
+
+
+
+
+
+
+//4
+// Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
+function countTotalSalary(employeesSalaries){
+employeesSalaries.values
+}
+const employeesSalaries = {
+    Andriy: 34000,
+    Olena: 103000,
+    Nazar: 78000,
+    Vlad: 0,
+    Anastasia: 59000,
+}
+console.log(obj.key);
+//5

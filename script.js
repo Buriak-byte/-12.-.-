118ff27b41ea33cfee6b7c6c2ed8f1fe48f106d7
Buriak-byte <oleksii.buriak12@gmail.com>
2026-04-17 21:49:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,51 +12,44 @@ for (const key of keys) {
   console.log(`${key}: ${user[key]}`);
 }
 //2
-// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
+
 const user2 = {
-car: "Toyota",
-home: "flat",
-placeOflive: "Kyiv",
-children: false,
-cat: true,
-countProps(obj){
-return Object.keys(obj).length;
-}
-}
-console.log(user2.countProps(user2))
+  car: "Toyota",
+  home: "flat",
+  placeOflive: "Kyiv",
+  children: false,
+  cat: true,
+  countProps(obj) {
+    return Object.keys(obj).length;
+  },
+};
+console.log(user2.countProps(user2));
 //3
-// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
+
 function findBestEmployee(employees) {
   let TheBestEmployerName = "";
-let TheBestEmployerNumber = 0
-const employeesValues = Object.entries(employees);
-for (const [employer, completedTasks] of employeesValues) {
-if(completedTasks > TheBestEmployerNumber){
-  TheBestEmployerNumber = completedTasks;
-    TheBestEmployerName = employer;
-}
-}
-return `${TheBestEmployerName}: ${TheBestEmployerNumber}`;
+  let TheBestEmployerNumber = 0;
+  const employeesValues = Object.entries(employees);
+  for (const [employer, completedTasks] of employeesValues) {
+    if (completedTasks > TheBestEmployerNumber) {
+      TheBestEmployerNumber = completedTasks;
+      TheBestEmployerName = employer;
+    }
+  }
+  return `${TheBestEmployerName}: ${TheBestEmployerNumber}`;
 }
 
 const employees = {
-    Andriy: 45,
-    Olena: 128,
-    Nazar: 150,
-    Vlad: 0,
-    Anastasia: 95,
-    
-}
+  Andriy: 45,
+  Olena: 128,
+  Nazar: 150,
+  Vlad: 0,
+  Anastasia: 95,
+};
 
 console.log(findBestEmployee(employees));
 
-
-
-
-
-
 //4
-// Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
 const employeesSalaries = {
   Andriy: 34000,
   Olena: 103000,
@@ -64,14 +57,48 @@ const employeesSalaries = {
   Vlad: 0,
   Anastasia: 59000,
 };
-function countTotalSalary(employeesSalaries){
-const values = Object.values(employeesSalaries)
- let sum = 0;
-for(let value = 0; value < values.length; value++){
-sum += values[value];
+function countTotalSalary(employeesSalaries) {
+  const values = Object.values(employeesSalaries);
+  const entries = Object.entries(employeesSalaries);
+  let sum = 0;
+  for (let value = 0; value < values.length; value++) {
+    sum += values[value];
+  }
+  for (const [employer2, Salary] of entries) {
+    console.log(`${employer2}: ${Salary}`);
+  }
+  return sum;
 }
-return sum
-}
-
 
 console.log(countTotalSalary(employeesSalaries));
+//5
+const arr = [
+  { prop: "tomato", exp: 10 },
+  { prop: "auto", exp: 2000 },
+  { prop: "lopast", exp: 50 },
+  { prop: "eight", exp: 8 },
+  { prop: "CSGO", exp: 0 },
+];
+
+function getAllPropValues(arr, prop) {
+  const result = [];
+  for (let i1 = 0; i1 < arr.length; i1++) {
+    result.push(arr[i1][prop]);
+  }
+  return result;
+}
+
+console.log(getAllPropValues(arr, "prop"))
+console.log(getAllPropValues(arr, "exp"))
+//6
+const allProducuts = [
+  { name: "milk", count: 3, price: 60 },
+]
+const productName = allProducuts[0].name;
+console.log(productName)
+function calculateTotalPrice(allProducuts, productName){
+  const productPrice = allProducuts[0].price
+  const productsCount = allProducuts[0].count
+  return productPrice * productsCount
+}
+console.log(calculateTotalPrice(allProducuts, productName));
